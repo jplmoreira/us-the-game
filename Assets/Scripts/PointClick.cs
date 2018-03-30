52d564a32241dfd1f34a52eba84d45240a6ccfb4
jplmoreira <jplmoreira92@gmail.com>
2018-03-30 15:52:20 +0100

    Fix point and click movement

--- Assets/Scripts/PointClick.cs
+++ Assets/Scripts/PointClick.cs
@@ -7,23 +7,33 @@ public class PointClick : MonoBehaviour {
     public int speed = 5;
     public LayerMask obstacles;
     private Vector2 targetPosition;
-	
-	// Update is called once per frame
-	void Update () {
+    private bool move = false;
+
+    // Update is called once per frame
+    void FixedUpdate () {
         Vector2 pos = transform.position;
         float y = pos.y;
 		if (Input.GetKeyDown(KeyCode.Mouse0)) {
-            Vector2 targetPos;
+            move = true;
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector2 relativePos = new Vector2(mousePos.x, y);
-            Vector2 dir = mousePos - pos;
-            RaycastHit2D hit = Physics2D.Raycast(pos, dir, 1000, obstacles);
+            Debug.Log("Transform[x: " + pos.x + ", y: " + y + "] Mouse[x: " + relativePos.x + ", y: " + relativePos.y + "]");
+            Vector2 dir = relativePos - pos;
+            Debug.Log("Direction[x: " + dir.x + ", y: " + dir.y + "]");
+            float distance = Vector2.Distance(pos, relativePos);
+            RaycastHit2D hit = Physics2D.Raycast(pos, dir, distance, obstacles);
             if (hit.collider != null) {
-                targetPos = hit.point;
+                targetPosition = hit.point;
             } else {
-                targetPos = relativePos;
+                targetPosition = relativePos;
             }
-            transform.position = Vector2.MoveTowards(transform.position, targetPos, Time.deltaTime * speed);
         }
-	}
+        if (move) {
+            transform.position = Vector2.MoveTowards(pos, targetPosition, Time.deltaTime * speed);
+            if (Vector2.Distance(pos, targetPosition) < 0.01) {
+                GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                move = false;
+            }
+        }    
+    }
 }
