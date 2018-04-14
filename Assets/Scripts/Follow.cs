2e029b621b2856b28622357e7618efb1e0d094ab
jplmoreira <jplmoreira92@gmail.com>
2018-04-14 05:22:51 +0100

    Add peek mechanic to camera

--- Assets/Scripts/Follow.cs
+++ Assets/Scripts/Follow.cs
@@ -6,6 +6,7 @@ public class Follow : MonoBehaviour {
 
     private GameObject player;       //Public variable to store a reference to the player game object
     private Vector3 offset;         //Private variable to store the offset distance between the player and camera
+    private bool focus = true;
 
     private void Awake() {
         GameObject[] characters = GameObject.FindGameObjectsWithTag("Character");
@@ -25,8 +26,8 @@ public class Follow : MonoBehaviour {
     // LateUpdate is called after Update each frame
     void LateUpdate() {
         // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-
-        transform.position = player.transform.position + offset;
+        if (focus)
+            transform.position = player.transform.position + offset;
     }
 
     public IEnumerator SwapFocus() {
@@ -41,4 +42,6 @@ public class Follow : MonoBehaviour {
             player = characters[0];
         }
     }
+
+    public void Focus(bool f) { focus = f; }
 }
---
+++ Assets/Scripts/Peek.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Peek : MonoBehaviour {
+
+    public float lookAheadFactor;
+    public float moveRate;
+
+    private Vector2 truePos;
+    private Vector2 targetPos;
+    private bool move = false;
+    private bool buttonDown = false;
+    private float z;
+
+	// Use this for initialization
+	void Awake () {
+        truePos = transform.position;
+        z = transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Mouse1) && !move) {
+            buttonDown = true;
+            GetComponent<Follow>().Focus(false);
+            Debug.Log("Right mouse down");
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 dir = mousePos - (Vector2) transform.position;
+            dir.Normalize();
+            targetPos = mousePos + dir * lookAheadFactor;
+            truePos = transform.position;
+            Debug.Log("Target: " + targetPos + " Pos: " + truePos);
+            move = true;
+        } else if (Input.GetKeyUp(KeyCode.Mouse1)) {
+            buttonDown = false;
+            GetComponent<Follow>().Focus(false);
+            Debug.Log("Right mouse up");
+            targetPos = truePos;
+            move = true;
+        }
+        if (move) {
+            Debug.Log("Moving...");
+            Vector2 newPos = Vector2.Lerp(truePos, targetPos, Time.deltaTime * 1);
+            Debug.Log("New pos: " + newPos);
+            transform.position = new Vector3(newPos.x, newPos.y, z);
+            if (Vector2.Distance(transform.position, targetPos) < 0.01) {
+                move = false;
+                if (!buttonDown)
+                    GetComponent<Follow>().Focus(true);
+            }
+        }
+    }
+}
