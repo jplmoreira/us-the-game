cc26f29d5ce28af2484661edbcffe5d87e7c95cd
martadmendes <marta.durand@gmail.com>
2018-04-14 22:15:42 +0100

    Update sprites

--- Assets/Scripts/PointClick.cs
+++ Assets/Scripts/PointClick.cs
@@ -18,6 +18,8 @@ public class PointClick : MonoBehaviour {
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector2 relativePos = new Vector2(mousePos.x, y);
             Vector2 dir = relativePos - pos;
+            //comparar posicao do rato com posicao do click
+            //mudar o sprite de acordo
             float distance = Vector2.Distance(pos, relativePos);
             RaycastHit2D hit = Physics2D.Raycast(pos, dir, distance, obstacles);
             if (hit.collider != null) {
@@ -27,10 +29,12 @@ public class PointClick : MonoBehaviour {
             }
         }
         if (move) {
+            //activar animaçao
             transform.position = Vector2.MoveTowards(pos, targetPosition, Time.deltaTime * speed);
             if (Vector2.Distance(pos, targetPosition) < 0.01) {
                 GetComponent<Rigidbody2D>().velocity = Vector2.zero;
                 move = false;
+                //voltar ao sprite normal/animaçao idle
             }
         }    
     }
