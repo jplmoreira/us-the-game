76605bd52c7c474c7c2911b25657539f47e8ca7f
martadmendes <marta.durand@gmail.com>
2018-04-15 04:47:51 +0100

    Update animations

--- Assets/Scripts/PointClick.cs
+++ Assets/Scripts/PointClick.cs
@@ -8,33 +8,46 @@ public class PointClick : MonoBehaviour {
     public LayerMask obstacles;
     private Vector2 targetPosition;
     private bool move = false;
+    Animator animator;
+    
+    // Initializing animation
+    void Start() {
+        animator = GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void FixedUpdate () {
         Vector2 pos = transform.position;
         float y = pos.y;
-		if (Input.GetKeyDown(KeyCode.Mouse0)) {
+
+        if (Input.GetKeyDown(KeyCode.Mouse0)) {
             move = true;
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector2 relativePos = new Vector2(mousePos.x, y);
             Vector2 dir = relativePos - pos;
-            //comparar posicao do rato com posicao do click
-            //mudar o sprite de acordo
+
+            if (dir.x < 0 && transform.localScale.x > 0 || dir.x > 0 && transform.localScale.x < 0) {
+                transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+            }
+
             float distance = Vector2.Distance(pos, relativePos);
             RaycastHit2D hit = Physics2D.Raycast(pos, dir, distance, obstacles);
+
             if (hit.collider != null) {
                 targetPosition = hit.point;
             } else {
                 targetPosition = relativePos;
             }
         }
+
         if (move) {
-            //activar animaçao
+            animator.SetBool("Walking", true);
             transform.position = Vector2.MoveTowards(pos, targetPosition, Time.deltaTime * speed);
             if (Vector2.Distance(pos, targetPosition) < 0.01) {
                 GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                animator.SetBool("Walking", false);
                 move = false;
-                //voltar ao sprite normal/animaçao idle
+                
             }
         }    
     }
