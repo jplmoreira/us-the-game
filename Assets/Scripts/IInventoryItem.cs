b4e6a34b5a43426b2d292fdeb570ec61268b0a59
jplmoreira <jplmoreira92@gmail.com>
2018-04-15 13:45:08 +0100

    Add initial inventory interaction

---
+++ Assets/Scripts/IInventoryItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IIventoryItem {
+
+    string Name { get; }
+    Sprite Image { get; }
+
+    void OnPickup();
+}
+
+public class InventoryEventArgs : EventArgs {
+    public InventoryEventArgs(IIventoryItem item) {
+        Item = item;
+    }
+
+    public IIventoryItem Item;
+}
---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    private const int SLOTS = 9;
+    private List<IIventoryItem> mItems = new List<IIventoryItem>();
+    public event EventHandler<InventoryEventArgs> ItemAdded;
+
+    public void AddItem(IIventoryItem item) {
+        if  (mItems.Count < SLOTS) {
+            // Add item to inventory
+        }
+    }
+}
