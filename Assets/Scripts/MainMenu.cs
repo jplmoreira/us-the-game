f1de5c28fb9f0d1006fabfe11aad22165b9df3cc
Telma <fc44796@alunos.fc.ul.pt>
2018-04-24 16:22:49 +0100

    added menu stuff

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+//https://www.youtube.com/watch?v=zc8ac_qUXQY     TUTORIAL
+
+public class MainMenu : MonoBehaviour {
+
+	public void PlayGame(){
+        //SceneManager.GetActiveScene().buildIndex()+1
+        SceneManager.LoadScene("Protoype");
+    }
+    
+    public void OptionsMenu(){
+        
+    }
+    
+}
