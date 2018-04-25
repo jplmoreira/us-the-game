d134c667e4f2ce0ebe239546dc3208d3fd0f9cf8
martadmendes <marta.durand@gmail.com>
2018-04-25 04:06:43 +0100

    Implement character swap (continue)

--- Assets/Scripts/CharacterSwap.cs
+++ Assets/Scripts/CharacterSwap.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class CharacterSwap : MonoBehaviour {
 
+    GameObject[] characters;
+    GameObject currChar;
+
     public Transform player1;
     private Vector3 pos1;
     public Transform player2;
@@ -41,89 +44,72 @@ public class CharacterSwap : MonoBehaviour {
         currNum = 1;
     }
 
-    //void swapChuck() {
-
-    //}
-    //void swapNewt() {
-
-//    }
-  //  void swapPiper() {
-
-    //}
-    //void swapWesley() {
-
-//    }
-    // Update is called once per frame
-    void Update () {
-        GameObject[] characters;
-        GameObject currChar;
-	    if (Input.GetKeyDown(KeyCode.Alpha1) && currNum != 1) {
-            characters = GameObject.FindGameObjectsWithTag("Character");
-            if (characters.Length != 1) {
-                Debug.LogError("More than one character Active!");
-                return;
-            }
-            currChar = characters[0];
-            SavePosition(currChar);
-            Destroy(currChar);
-            Transform aux = Instantiate(player1, pos1, Quaternion.identity);
-			currP = aux.gameObject;
-            currNum = 1;
-            StartCoroutine(cam.GetComponent<Follow>().SwapFocus());
-			if (CharSwap != null) {
-				CharSwap (this, new CharacterSwapArgs (currNum));
-			}
+    public void swapChuck() {
+        characters = GameObject.FindGameObjectsWithTag("Character");
+        if (characters.Length != 1) {
+            Debug.LogError("More than one character Active!");
+            return;
         }
-        if (Input.GetKeyDown(KeyCode.Alpha2) && currNum != 2) {
-            characters = GameObject.FindGameObjectsWithTag("Character");
-            if (characters.Length != 1) {
-                Debug.LogError("More than one character Active!");
-                return;
-            }
-            currChar = characters[0];
-            SavePosition(currChar);
-            Destroy(currChar);
-			Transform aux = Instantiate(player2, pos2, Quaternion.identity);
-			currP = aux.gameObject;
-            currNum = 2;
-            StartCoroutine(cam.GetComponent<Follow>().SwapFocus());
-			if (CharSwap != null) {
-				CharSwap (this, new CharacterSwapArgs (currNum));
-			}
+        currChar = characters[0];
+        SavePosition(currChar);
+        Destroy(currChar);
+        Transform aux = Instantiate(player4, pos4, Quaternion.identity);
+        currP = aux.gameObject;
+        currNum = 4;
+        StartCoroutine(cam.GetComponent<Follow>().SwapFocus());
+        if (CharSwap != null) {
+            CharSwap(this, new CharacterSwapArgs(currNum));
         }
-        if (Input.GetKeyDown(KeyCode.Alpha3) && currNum != 3) {
-            characters = GameObject.FindGameObjectsWithTag("Character");
-            if (characters.Length != 1) {
-                Debug.LogError("More than one character Active!");
-                return;
-            }
-            currChar = characters[0];
-            SavePosition(currChar);
-            Destroy(currChar);
-			Transform aux = Instantiate(player3, pos3, Quaternion.identity);
-			currP = aux.gameObject;
-            currNum = 3;
-            StartCoroutine(cam.GetComponent<Follow>().SwapFocus());
-			if (CharSwap != null) {
-				CharSwap (this, new CharacterSwapArgs (currNum));
-			}
+    }
+    public void swapNewt() {
+        characters = GameObject.FindGameObjectsWithTag("Character");
+        if (characters.Length != 1) {
+            Debug.LogError("More than one character Active!");
+            return;
         }
-        if (Input.GetKeyDown(KeyCode.Alpha4) && currNum != 4) {
-            characters = GameObject.FindGameObjectsWithTag("Character");
-            if (characters.Length != 1) {
-                Debug.LogError("More than one character Active!");
-                return;
-            }
-            currChar = characters[0];
-            SavePosition(currChar);
-            Destroy(currChar);
-			Transform aux = Instantiate(player4, pos4, Quaternion.identity);
-			currP = aux.gameObject;
-            currNum = 4;
-            StartCoroutine(cam.GetComponent<Follow>().SwapFocus());
-			if (CharSwap != null) {
-				CharSwap (this, new CharacterSwapArgs (currNum));
-			}
+        currChar = characters[0];
+        SavePosition(currChar);
+        Destroy(currChar);
+        Transform aux = Instantiate(player2, pos2, Quaternion.identity);
+        currP = aux.gameObject;
+        currNum = 2;
+        StartCoroutine(cam.GetComponent<Follow>().SwapFocus());
+        if (CharSwap != null) {
+            CharSwap(this, new CharacterSwapArgs(currNum));
+        }
+    }
+    public void swapPiper() {
+        characters = GameObject.FindGameObjectsWithTag("Character");
+        if (characters.Length != 1) {
+            Debug.LogError("More than one character Active!");
+            return;
+        }
+        currChar = characters[0];
+        SavePosition(currChar);
+        Destroy(currChar);
+        Transform aux = Instantiate(player3, pos3, Quaternion.identity);
+        currP = aux.gameObject;
+        currNum = 3;
+        StartCoroutine(cam.GetComponent<Follow>().SwapFocus());
+        if (CharSwap != null) {
+            CharSwap(this, new CharacterSwapArgs(currNum));
+        }
+    }
+    public void swapWesley() {
+        characters = GameObject.FindGameObjectsWithTag("Character");
+        if (characters.Length != 1) {
+            Debug.LogError("More than one character Active!");
+            return;
+        }
+        currChar = characters[0];
+        SavePosition(currChar);
+        Destroy(currChar);
+        Transform aux = Instantiate(player1, pos1, Quaternion.identity);
+        currP = aux.gameObject;
+        currNum = 1;
+        StartCoroutine(cam.GetComponent<Follow>().SwapFocus());
+        if (CharSwap != null) {
+            CharSwap(this, new CharacterSwapArgs(currNum));
         }
     }
 
