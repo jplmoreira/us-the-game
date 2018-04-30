d96242d8b24600b1de1dbf7d13e71c2687333a9a
martadmendes <marta.durand@gmail.com>
2018-04-30 17:12:18 +0100

    Fix switch cases in stress bar script

--- Assets/Scripts/StressBar.cs
+++ Assets/Scripts/StressBar.cs
@@ -40,6 +40,7 @@ public class StressBar : MonoBehaviour {
                 if (currentStress < 0) {
                     currentStress = 0;
                     stressBar.sizeDelta = new Vector2(currentStress, stressBar.sizeDelta.y);
+                    break;
                 }
             case 25:
                 if (currentStress < 25) {
