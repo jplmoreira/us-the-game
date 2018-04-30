75b8f9639811a510a092eaad2ff5de7f69810bbb
jplmoreira <jplmoreira92@gmail.com>
2018-04-30 17:27:57 +0100

    Add trapdoor and fix camera position

--- Assets/Scripts/Stone.cs
+++ Assets/Scripts/Stone.cs
@@ -6,5 +6,10 @@ public class Stone : MonoBehaviour {
 
     private void OnCollisionEnter2D(Collision2D collision) {
         Destroy(gameObject, 2f);
+        if (collision.transform.tag.Equals("Hittable")) {
+            Animator anim = collision.transform.gameObject.GetComponent<Animator>();
+            if (anim != null)
+                anim.SetBool("hit", true);
+        }
     }
 }
