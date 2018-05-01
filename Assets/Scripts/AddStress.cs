cf99033e946dbe4441bb3874842cfdbe2980eb71
jplmoreira <jplmoreira92@gmail.com>
2018-05-02 00:10:37 +0100

    Add placeholder room

---
+++ Assets/Scripts/AddStress.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddStress : Interactable {
+
+    public int amount;
+
+	public void Interact_ChangeStress() {
+        if (amount > 0)
+            StressBar.ins.increment(amount);
+        else
+            StressBar.ins.decrement(-amount);
+    }
+}
