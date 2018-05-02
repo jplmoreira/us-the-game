9457781e3d9f13af595ccbb59e5b723c5355ec55
jplmoreira <jplmoreira92@gmail.com>
2018-05-02 16:37:13 +0100

    Revamp Reachable method and fix item colliders

--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class Interact : MonoBehaviour {
 
 	public float reach;
-	public LayerMask mask;
+	public LayerMask layerMask;
 
     public bool Reachable(GameObject obj) {
         Transform currP = CharacterSwap.ins.currP.transform;
         Vector3 pos = new Vector3(currP.position.x, currP.position.y - 0.3f, currP.position.z);
-        Vector3 dir = obj.transform.position - pos;
-        dir.Normalize();
-        RaycastHit2D hit = Physics2D.Raycast (pos, dir, reach, mask);
-		if (hit.collider != null && hit.transform.tag == "Item") {
-			return true;
+		Vector3 dir = obj.transform.position - pos;
+		dir.Normalize();
+		RaycastHit2D[] hits = Physics2D.RaycastAll (pos, dir, reach, layerMask);
+		foreach (RaycastHit2D hit in hits) {
+			string title = obj.gameObject.GetComponent<Interactable> ().title;
+			if (hit.transform.tag.Equals("Floor")) {
+				return false;
+			} else if (hit.transform.gameObject.GetComponent<Interactable> ().title.Equals (title)) {
+				return true;
+			}
 		}
 		return false;
 	}
