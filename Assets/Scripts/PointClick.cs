19329ebeef0266369a0511b31ab1d77cbd6b8d6a
jplmoreira <jplmoreira92@gmail.com>
2018-05-22 19:36:19 +0100

    Add movement overhaul and update walking animation

--- Assets/Scripts/PointClick.cs
+++ Assets/Scripts/PointClick.cs
@@ -4,64 +4,63 @@ using UnityEngine;
 
 public class PointClick : MonoBehaviour {
 
-    public int speed = 5;
-    public LayerMask obstacles;
-    private Vector2 targetPosition;
-    private bool move = false;
+    public int speed;
     Animator animator;
-    //public AudioManager audioManager;
+    private float targetX;
+    private Rigidbody2D rb;
+    private Vector2 lastPos;
+    private float timePassed = 0f;
+    private Vector2 dir = Vector2.right;
     [HideInInspector]
     public bool interacting;
     
     // Initializing animation
     void Start() {
-        animator = GetComponent<Animator>();
         interacting = false;
-        GetComponent<Rigidbody2D>().constraints |= RigidbodyConstraints2D.FreezeRotation;
-        
-        //audioManager.Play("LevelBackgroundMusic");
+        lastPos = transform.position;
+        targetX = transform.position.x;
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        rb.constraints |= RigidbodyConstraints2D.FreezeRotation;
+    }
 
+    private void Update() {
+        if (!interacting) {
+            if (Input.GetKey(KeyCode.Mouse1)) {
+                Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                if (mousePos.x - transform.position.x < 0 && transform.localScale.x > 0 ||
+                    mousePos.x - transform.position.x > 0 && transform.localScale.x < 0) {
+                    transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+                    dir = new Vector2(dir.x * -1, 0);
+                }
+                targetX = mousePos.x;
+            }
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate () {
         if (!interacting) {
-            Vector2 pos = transform.position;
-            float y = pos.y;
-
-            if (Input.GetKeyDown(KeyCode.Mouse1)) {
-                move = true;
-                Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                Vector2 relativePos = new Vector2(mousePos.x, y);
-                Vector2 dir = relativePos - pos;
-
-                if (dir.x < 0 && transform.localScale.x > 0 || dir.x > 0 && transform.localScale.x < 0) {
-                    transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
-                }
-
-                float distance = Vector2.Distance(pos, relativePos);
-                RaycastHit2D hit = Physics2D.Raycast(pos, dir, distance, obstacles);
-
-                if (hit.collider != null) {
-                    targetPosition = hit.point;
-                } else {
-                    targetPosition = relativePos;
-                }
-            }
-
-            if (move) {
-                animator.SetBool("Walking", true);
-                transform.position = Vector2.MoveTowards(pos, targetPosition, Time.deltaTime * speed);
-                if (Vector2.Distance(pos, targetPosition) < 0.01) {
-                    GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-                    animator.SetBool("Walking", false);
-                    move = false;
-                }
+            float dis = Mathf.Abs(targetX - transform.position.x);
+            if (dis < 0.15) {
+                StopMoving();
+            } else if (Mathf.Abs(rb.velocity.x) <= speed) {
+                rb.AddForce(dir * 1000, ForceMode2D.Force);
             }
         }
+        if (timePassed > 0.5f) {
+            if (lastPos.x == transform.position.x)
+                StopMoving();
+            else
+                lastPos = transform.position;
+            timePassed = 0f;
+        }
+        timePassed += Time.deltaTime;
+        animator.SetFloat("speed", Mathf.Abs(rb.velocity.x));
     }
 
     public void StopMoving() {
-        targetPosition = transform.position;
+        targetX = transform.position.x;
+        rb.velocity = new Vector2(0, rb.velocity.y);
     }
 }
