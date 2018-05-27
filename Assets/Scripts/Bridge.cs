adf0a75ab657760dad6b817644a1f952e0521243
jplmoreira <jplmoreira92@gmail.com>
2018-05-27 19:06:38 +0100

    Add bridge to basement

---
+++ Assets/Scripts/Bridge.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bridge : Interactable {
+
+    bool opened = false;
+
+    public GameObject hole;
+
+	public void Interact_Push() {
+        if (CharacterSwap.ins.currP.name.Contains("Player4")) {
+            if (!opened) {
+                GetComponent<BoxCollider2D>().enabled = false;
+                GetComponent<Animator>().SetBool("opened", true);
+                hole.SetActive(false);
+            }
+        } else
+            DialogueManager.ins.NewDialogue("I am not strong enough to push this!");
+    }
+}
