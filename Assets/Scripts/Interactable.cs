39e14d4b1fedec94bcd022f6da89a47aa9872fd5
jplmoreira <jplmoreira92@gmail.com>
2018-05-27 03:09:51 +0100

    Add cursor change when hovering interactables

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -5,22 +5,29 @@ using UnityEngine;
 public abstract class Interactable : MonoBehaviour {
     public string title;
 
-	public bool canInteract = true;
+    public bool canInteract = true;
+    private Texture2D cursor;
 
 	// Use this for initialization
 	void Start () {
 		if (title == "" || title == null ) {
             title = gameObject.name;
         }
-	}
+        cursor = (Texture2D)Resources.Load("interact", typeof(Texture2D));
+    }
 	
 	// Update is called once per frame
 	void OnMouseOver () {
-		if (Input.GetMouseButtonDown(0)) {
+        Cursor.SetCursor(cursor, Vector2.zero, CursorMode.Auto);
+        if (Input.GetMouseButtonDown(0)) {
             if (CharacterSwap.ins.currP.GetComponent<Interact>().Reachable(this.gameObject))
                 RadialMenuSpawner.ins.SpawnMenu(this);
             else
                 Debug.Log("Out of reach!");
         }
 	}
+
+    private void OnMouseExit() {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
 }
